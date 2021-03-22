91ec09ff6db5fb273a316926858bbf5d40290a99
Harri <har.grace@outlook.com>
2021-03-22 19:42:40 +0000

    Fixed tokenizer for multiple brackets

--- Week5-7 DB/src/DBTokenizer.java
+++ Week5-7 DB/src/DBTokenizer.java
@@ -15,8 +15,20 @@ public class DBTokenizer {
     //Convert command string to array for better handling
     public void setToken(String command){
 
+        int i, j;
+
         command = editOpenBracket(command);
+
+        for(i = 0; i < command.length(); i++) {
+            System.out.println(command);
+        }
+
         command = editCloseBracket(command);
+
+        for(j = 0; j < command.length(); j++) {
+            System.out.println(command);
+        }
+
         command = editSemiColon(command);
 
         tokenArray = command.split(" ");
@@ -25,22 +37,34 @@ public class DBTokenizer {
 
     //Method to edit start bracket if it exists
     public String editOpenBracket(String command){
-        if(command.indexOf("(") != -1){
-            return command.substring(0, command.indexOf("(") + 1) + " " + command.substring(command.indexOf("(") + 1);
+
+        int i, index;
+        index = command.indexOf("(");
+
+        if(index != -1){
+            for(i = 0; i < (stringSpecicalCount(command, '(')); i++){
+                command = command.substring(0, index + 1) + " " + command.substring(index + 1);
+                index = command.indexOf("(", index + 1);
+            }
         }
-        else{
-            return command;
-        }
+        return command;
     }
 
     //Method to edit ending bracket if it exists
     public String editCloseBracket(String command){
-        if(command.indexOf(")") != -1){
-            return command.substring(0, command.indexOf(")")) + " " + command.substring(command.indexOf(")"));
+
+        int i, index;
+        index = command.indexOf(")");
+
+        if(index != -1){
+            for(i = 0; i < (stringSpecicalCount(command, ')')); i++){
+                command = command.substring(0, index) + " " + command.substring(index);
+
+                //Plus two to include new space
+                index = command.indexOf(")", index + 2);
+            }
         }
-        else{
-            return command;
-        }
+        return command;
     }
 
     //Method to edit semi-colon
@@ -53,4 +77,16 @@ public class DBTokenizer {
         }
     }
 
+    //Method to count how many signs match parameter in string
+    public int stringSpecicalCount(String command, char symbol){
+
+        int j, count = 0;
+
+        for(j = 0; j < command.length(); j++){
+            if(command.charAt(j) == symbol){
+                count++;
+            }
+        }
+        return count;
+    }
 }

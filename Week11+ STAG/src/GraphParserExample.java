e2646a8620b67a03ed739005c5da4898df4b9c27
Harri <har.grace@outlook.com>
2021-04-24 14:27:12 +0100

    Took out prints

--- Week11+ STAG/src/GraphParserExample.java
+++ Week11+ STAG/src/GraphParserExample.java
@@ -1,3 +1,4 @@
+import STAGData.LocationData;
 import com.alexmerz.graphviz.*;
 import com.alexmerz.graphviz.objects.*;
 import java.io.FileNotFoundException;
@@ -23,23 +24,25 @@ public class GraphParserExample {
             ArrayList<Graph> graphs = parser.getGraphs();
             ArrayList<Graph> subGraphs = graphs.get(0).getSubgraphs();
             for(Graph g : subGraphs){
-                System.out.printf("id = %s\n",g.getId().getId());
+                //System.out.printf("id = %s\n",g.getId().getId());
 
                 ArrayList<Graph> subGraphs1 = g.getSubgraphs();
                 for (Graph g1 : subGraphs1){
                     ArrayList<Node> nodesLoc = g1.getNodes(false);
                     Node nLoc = nodesLoc.get(0);
-                    System.out.printf("\tid = %s, name = %s\n, description = %s\n",g1.getId().getId(), nLoc.getId().getId(), nLoc.getAttribute("description"));
+                    //System.out.printf("\tid = %s, name = %s\n, description = %s\n",g1.getId().getId(), nLoc.getId().getId(), nLoc.getAttribute("description"));
 
                     //Create a new class in array for each location
                     LocationData loc = new LocationData(nLoc.getId().getId(), nLoc.getAttribute("description"));
 
                     ArrayList<Graph> subGraphs2 = g1.getSubgraphs();
                     for (Graph g2 : subGraphs2) {
-                        System.out.printf("\t\tid = %s\n", g2.getId().getId());
+                        //System.out.printf("\t\tid = %s\n", g2.getId().getId());
                         ArrayList<Node> nodesEnt = g2.getNodes(false);
                         for (Node nEnt : nodesEnt) {
-                            System.out.printf("\t\t\tid = %s, description = %s\n", nEnt.getId().getId(), nEnt.getAttribute("description"));
+                            //System.out.printf("\t\t\tid = %s, description = %s\n", nEnt.getId().getId(), nEnt.getAttribute("description"));
+
+                            //Store the information into the respective location class
                             if(g2.getId().getId().equals("artefacts")){
                                 loc.addArtefact(nEnt.getId().getId(), nEnt.getAttribute("description"));
                             }
@@ -51,13 +54,16 @@ public class GraphParserExample {
                             }
                         }
                     }
+
+                    //Add location data to arraylist of locations
                     location.add(loc);
-                    //Issue is how do we add paths if it's separate to the creation of the location?
                 }
 
                 ArrayList<Edge> edges = g.getEdges();
                 for (Edge e : edges){
-                    System.out.printf("Path from %s to %s\n", e.getSource().getNode().getId().getId(), e.getTarget().getNode().getId().getId());
+                    //System.out.printf("Path from %s to %s\n", e.getSource().getNode().getId().getId(), e.getTarget().getNode().getId().getId());
+
+                    //Look through locations and store the path if mentioned
                     for(i=0; i < location.size(); i++){
                         if(e.getSource().getNode().getId().getId().equals(location.get(i).getLoc())){
                             location.get(i).addPath(e.getTarget().getNode().getId().getId());
